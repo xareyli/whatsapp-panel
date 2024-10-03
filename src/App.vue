0d002177972cc73cb3f10c4973c17d7136618854
xareyli <xareyli@gmail.com>
2024-10-03 23:56:18 +0300

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import whatsappIcon from '@/assets/whatsappsvg.svg'
+</script>
+
+<template>
+    <header class="bg-gray flex items-center h-20 text-white">
+        <div class="mx-auto w-[1240px]">
+            <div class="text-lg flex justify-start items-center">
+                <img :src="whatsappIcon" class="w-8 h-auto mr-4" /> WhatsApp Panel
+            </div>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: [],
+    theme: {
+        colors: {
+            gray: '#212529',
+            white: '#FFF',
+            red: '#FF0000',
+            orange: '#FFA900'
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif']
+        },
+        extend: {
+            spacing: {
+                128: '32rem',
+                144: '36rem'
+            },
+            borderRadius: {
+                '4xl': '2rem'
+            }
+        }
+    },
+    plugins: []
+}
